2b2650ae8e6e60038b590cbbd7218f41e66da520
Jonathan Kulak <lackadaize@msn.com>
2023-12-06 13:00:23 -0500

    This is the git commit for the Scope Lab assignment.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution in this file!
+// global
+var customerName = 'bob';
+
+// upperCaseCustomerName
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+// console.log(upperCaseCustomerName());
+
+// setBestCustomer
+function setBestCustomer() {
+    bestCustomer = "not bob";
+    return bestCustomer;
+}
+
+// console.log(setBestCustomer());
+
+// overwriteBestCustomer
+function overwriteBestCustomer () {
+  bestCustomer = "maybe bob";
+  return bestCustomer;
+}
+
+// console.log(overwriteBestCustomer());
+
+// changeLeastFavoriteCustomer
+const leastFavoriteCustomer = "Some Initial Value";
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "Some Changed Value";
+  return leastFavoriteCustomer;
+}
+
+// console.log(changeLeastFavoriteCustomer());
